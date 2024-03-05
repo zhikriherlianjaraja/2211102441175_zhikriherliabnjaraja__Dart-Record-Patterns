e1f8c24eb51d19f590c96188f0a1c2ef03c8644a
zhikriherlianjaraja <zhikriherlianjaraja>
2024-03-05 10:14:19 +0800

    commite

---
+++ lib/data.dart
@@ -0,0 +1,37 @@
+import 'dart:convert';
+
+class Document {
+  final Map<String, Object?> _json;
+  Document() : _json = jsonDecode(documentJson);
+
+  (String, {DateTime modified}) getMetadata() {
+    var title = "My Document";
+    var now = DateTime.now();
+
+    return (title, modified: now);
+  }
+}
+
+const documentJson = '''
+{
+  "metadata": {
+    "title": "My Document",
+    "modified": "2023-05-10"
+  },
+  "blocks": [
+    {
+      "type": "h1",
+      "text": "Chapter 1"
+    },
+    {
+      "type": "p",
+      "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+      "type": "checkbox",
+      "checked": false,
+      "text": "Learn Dart 3"
+    }
+  ]
+}
+''';
